db37d1d6a9e2ec0827975be1d5ecc75b29bf90b5
Cheuk Lam Cheung <ccheung792423@gmail.com>
2021-02-13 15:05:07 -0500

    navbar

---
+++ script.js
@@ -0,0 +1,48 @@
+// // When the user scrolls the page, execute myFunction
+// window.onscroll = function() {
+//     myFunction()
+// };
+
+// // Get the navbar
+// var navbar = document.getElementById("navbar");
+
+// // Get the offset position of the navbar
+// var sticky = navbar.offsetTop;
+
+// // Add the sticky class to the navbar when you reach its scroll position. Remove "sticky" when you leave the scroll position
+// function myFunction() {
+//     if (window.pageYOffset >= sticky) {
+//         navbar.classList.add("sticky")
+//     } 
+//     else {
+//         navbar.classList.remove("sticky");
+//     }
+// }
+
+
+
+// document.addEventListener('DOMContentLoaded',console.log("loaded"));
+
+// When the user scrolls the page, execute myFunction
+// window.onscroll = function() {
+//     stickyBar();
+//     console.log("this part works");
+// };
+
+// // Get the navbar
+// var navbar = document.getElementById("nav-link-wrapper");
+// console.log(navbar);
+
+// // Get the offset position of the navbar
+// var sticky = navbar.offsetTop;
+// console.log(sticky);
+
+// // Add the sticky class to the navbar when you reach its scroll position. Remove "sticky" when you leave the scroll position
+// function stickyBar() {
+//   if (window.pageYOffset >= sticky) {
+//     navbar.classList.add("sticky");
+//     console.log("adding sticky");
+//   } else {
+//     navbar.classList.remove("sticky");
+//   }
+// }
