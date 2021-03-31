ba6e56000eb4ab1d626915c46adade6acb943909
Cheuk Lam Cheung <ccheung792423@gmail.com>
2021-03-30 20:41:17 -0400

    created product.js

--- src/script.js
+++ src/script.js
@@ -98,67 +98,70 @@ function checkValid(inputElement){
 document.addEventListener("DOMContentLoaded", () => {
     const loginForm = document.querySelector("#login");
     const createForm = document.querySelector("#signup");
-    // Switch between login and sign up page
-    document.querySelector("#signUpLink").addEventListener("click", e => {
-        // prevent the page from reloading when the link is pressed for smoother transition
-        e.preventDefault();
-        loginForm.classList.add("formHidden");
-        createForm.classList.remove("formHidden");
-    });
-    document.querySelector("#loginLink").addEventListener("click", e => {
-        e.preventDefault();
-        loginForm.classList.remove("formHidden");
-        createForm.classList.add("formHidden");
-    });
-    // check if the correct username/password combination is used
-    // username -> admin, password -> password
-    let attempt = 0;
-    loginForm.addEventListener("submit", e => {
-        e.preventDefault();
-        // hardset a username and password that works
-        if (loginForm.querySelector(".username").value.length > 0 && loginForm.querySelector(".password").value === "password"){
-            username = loginForm.querySelector(".username").value;
-            loggedin = true;
-            window.location.href = "home.html";
-        }
-        else {
-            // let the user know how to login after 5 attempts
-            if (attempt < 5){
-                setFormMessage(loginForm, "Incorrect username/password combination");
-                attempt++;
+    // Run when it is on the login page
+    if (loginForm !== null && createForm !== null){
+        // Switch between login and sign up page
+        document.querySelector("#signUpLink").addEventListener("click", e => {
+            // prevent the page from reloading when the link is pressed for smoother transition
+            e.preventDefault();
+            loginForm.classList.add("formHidden");
+            createForm.classList.remove("formHidden");
+        });
+        document.querySelector("#loginLink").addEventListener("click", e => {
+            e.preventDefault();
+            loginForm.classList.remove("formHidden");
+            createForm.classList.add("formHidden");
+        });
+        // check if the correct username/password combination is used
+        // username -> admin, password -> password
+        let attempt = 0;
+        loginForm.addEventListener("submit", e => {
+            e.preventDefault();
+            // hardset a username and password that works
+            if (loginForm.querySelector(".username").value.length > 0 && loginForm.querySelector(".password").value === "password"){
+                username = loginForm.querySelector(".username").value;
+                loggedin = true;
+                window.location.href = "home.html";
             }
             else {
-                setFormMessage(loginForm, "Put password as \"password\" :)");
+                // let the user know how to login after 5 attempts
+                if (attempt < 5){
+                    setFormMessage(loginForm, "Incorrect username/password combination");
+                    attempt++;
+                }
+                else {
+                    setFormMessage(loginForm, "Put password as \"password\" :)");
+                }
             }
-        }
-    });
-    createForm.addEventListener("submit", e => {
-        e.preventDefault();
-        // track if any fields are invalid
-        requirement = true;
-        createForm.querySelectorAll(".loginInput").forEach(inputElement => {
-            if (!checkValid(inputElement)){
-                requirement = false;
-            }
-        })
-        // redirect the person if everything is valid
-        if (requirement){
-            username = createForm.querySelector(".username").value;
-            loggedin = true;
-            window.location.href = "home.html";
-        }
-        else{
-            setFormMessage(createForm, "Requirements are not fullfilled");
-        }
-    });
-
-    document.querySelectorAll(".loginInput").forEach(inputElement => {
-        // check the validity of the input when the user moves on
-        inputElement.addEventListener("blur", e => {
-            checkValid(inputElement);
         });
-    });
+        createForm.addEventListener("submit", e => {
+            e.preventDefault();
+            // track if any fields are invalid
+            requirement = true;
+            createForm.querySelectorAll(".loginInput").forEach(inputElement => {
+                if (!checkValid(inputElement)){
+                    requirement = false;
+                }
+            })
+            // redirect the person if everything is valid
+            if (requirement){
+                username = createForm.querySelector(".username").value;
+                loggedin = true;
+                window.location.href = "home.html";
+            }
+            else{
+                setFormMessage(createForm, "Requirements are not fullfilled");
+            }
+        });
+        document.querySelectorAll(".loginInput").forEach(inputElement => {
+            // check the validity of the input when the user moves on
+            inputElement.addEventListener("blur", e => {
+                checkValid(inputElement);
+            });
+        });
+    }
 });
+
 
 
 
